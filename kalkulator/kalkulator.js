b767a528b920df12307f153c61c691fbef61f798
GodsEnWrath <fastyazid@gmail.com>
2019-11-05 17:14:08 +0700

    nambah kalkulator

--- kalkulator/kalkulator.js
+++ kalkulator/kalkulator.js
@@ -1,41 +1,64 @@
-function kalkulator()
-{
-//variable angka satu
-var angkaSatu = parseInt(document.getElementById(“angkaSatu”).value);
-//variable angka dua
-var angkaDua = parseInt(document.getElementById(“angkaDua”).value);
-//variable operasi aritmatik
-var op = document.getElementById(“op”).value;
-//variable angka hasil
-var hasil = document.getElementById(“hasil”);
-//variable untuk total angka satu dan angka dua
-var total;
+function add() {
+  let x = prompt("Masukkan bilangan pertama untuk pertambahan"),
+    y = prompt("Masukkan bilangan kedua untuk pertambahan"),
+    res = parseInt(x) + parseInt(y),
+    show = `Hasil ${x} - ${y} = ${res}`;
 
-if(isNaN(angkaSatu) || isNaN(angkaDua)){
-
-alert(‘isi form dengan angka!’)
-
-}else{
-
-if(op==”+”)
-{
-total = angkaSatu+angkaDua;
-}
-else if(op==”-”)
-{
-total = angkaSatu-angkaDua;
-}
-else if(op==”x”)
-{
-total = angkaSatu*angkaDua;
-}
-else
-{
-total = angkaSatu/angkaDua;
+  alert(show);
 }
 
+function subtract() {
+  let x = prompt("Masukkan bilangan pertama untuk pengurangan"),
+    y = prompt("Masukkan bilangan kedua untuk pengurangan"),
+    res = parseInt(x) - parseInt(y),
+    show = `Hasil ${x} - ${y} = ${res}`;
+
+  alert(show);
 }
 
-hasil.value = total;
+function multiply() {
+  let x = prompt("Masukkan bilangan pertama untuk perkalian"),
+    y = prompt("Masukkan bilangan kedua untuk perkalian"),
+    res = parseInt(x) * parseInt(y),
+    show = `Hasil ${x} x ${y} = ${res}`;
 
+  alert(show);
 }
+
+function divide() {
+  let x = prompt("Masukkan bilangan pertama untuk pembagian"),
+    y = prompt("Masukkan bilangan kedua untuk pembagian"),
+    res = parseInt(x) / parseInt(y),
+    show = `Hasil ${x} : ${y} = ${res}`;
+
+  alert(show);
+}
+
+function calculator() {
+  choice = prompt(
+    "Masukkan pilihan operasi yang Anda inginkan (1: pertambahan, 2: pengurangan, 3: perkalian, 4: pembagian"
+  );
+
+  switch (choice) {
+    case "1":
+      add();
+      break;
+    case "2":
+      subtract();
+      break;
+    case "3":
+      multiply();
+      break;
+    case "4":
+      divide();
+      break;
+  }
+
+  let retry = prompt("Apakah anda ingin mengulang (Yes/No)?");
+
+  if (retry === "Yes" || retry === "yes") {
+    calculator();
+  }
+}
+
+calculator();
